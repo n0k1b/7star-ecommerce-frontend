76ba78bb1c89baa55b857866bcc57ba0734c1013
abtahi-tajwar <abtahitajwar@gmail.com>
2021-11-26 00:11:21 +0600

    Product details page created

--- assets/js/style.js
+++ assets/js/style.js
@@ -66,6 +66,16 @@ function selectBadge() {
         })
     })
 }
+function matchHeight() {
+    // To create matching height and width of element
+    var match_height = document.querySelectorAll(".match_height");
+    if(match_height.length !== 0) {
+        match_height.forEach(elem => {
+            elem.style.height = elem.clientWidth + "px"
+        })
+    }
+}
 selectBadge()
 createStarRatingView()
 makeFavouriteButton()
+matchHeight()
